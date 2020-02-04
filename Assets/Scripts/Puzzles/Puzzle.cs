ef4bd744426e05adf51c8d90a4a9899b112f9c51
VelluVu <velluv@hotmail.fi>
2020-02-04 06:43:03 +0200

    Some changes and begin of puzzles

---
+++ Assets/Scripts/Puzzles/Puzzle.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Base class for puzzles
+/// </summary>
+public abstract class Puzzle : MonoBehaviour
+{
+    public string completionEventCode;
+    public string resetEventCode;
+
+    public float timeStep = 3.0f;
+    private IEnumerator coroutine;
+    public List<PuzzlePart> puzzleParts = new List<PuzzlePart>();
+    public bool finished = false;
+  
+
+    public IEnumerator Tick(float time)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeStep);
+
+            if (DebugTable.PuzzleDebug)
+                Debug.Log("Check Puzzle Status " + Time.time);
+
+            CheckStatus();
+        }
+    }
+
+    public void ResetPuzzle()
+    {
+        for (int i = 0; i < puzzleParts.Count; i++)
+        {
+            if (puzzleParts[i] != null)
+                puzzleParts[i].ResetPart();
+        }
+
+        finished = false;
+    }
+
+    public void CheckStatus()
+    {
+
+        if (puzzleParts.Any() == false)
+        {
+            if (DebugTable.PuzzleDebug)
+                Debug.LogError("EMPTY PUZZLE! " + this.name);
+
+            return;
+        }
+
+        for (int i = 0; i < puzzleParts.Count; i++)
+        {
+            if (puzzleParts[i] == null)
+            {
+                if (DebugTable.PuzzleDebug)
+                    Debug.LogError("Puzzle Part Index Null in : " + this.gameObject.name + " gameobject ! index : " + i);
+
+                return;
+            }
+
+            if (!puzzleParts[i].completed)
+            {
+                return;
+            }
+        }
+
+        finished = true;
+        EventManager.OnPuzzleCompleteEvent(completionEventCode);
+
+    }
+
+
+
+}
