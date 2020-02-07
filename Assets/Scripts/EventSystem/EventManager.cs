7377b682b9a49b81a829873d1a5f760202239720
VelluVu <velluv@hotmail.fi>
2020-02-07 08:00:21 +0200

    More Events
    Added more Generic events to call from Event Manager

--- Assets/Scripts/EventSystem/EventManager.cs
+++ Assets/Scripts/EventSystem/EventManager.cs
@@ -42,18 +42,53 @@ public class EventManager : MonoBehaviour
     public static event BasicEventDelegate onBasicEvent;
 
     /// <summary>
-    /// Generic One Parameter delegate
+    /// Generic One Parameter Delegate, in the end of the event name there are first letter of each parameter
     /// </summary>
-    /// <typeparam name="T">Generic Return type</typeparam>
+    /// <typeparam name="T">Generic T</typeparam>
     /// <param name="parameter">Generic parameter</param>
     /// <returns></returns>
-    public delegate T GenericDelegate<T>(T parameter);
-    public static event GenericDelegate<int> onSingleIntParameterEvent;
-    public static event GenericDelegate<float> onSingleFloatParameterEvent;
-    public static event GenericDelegate<string> onSingleStringParameterEvent;
-    public static event GenericDelegate<Vector3> onSingleVector3ParameterEvent;
-    public static event GenericDelegate<GameObject> onSingleGameObjectParameterEvent;
-    public static event GenericDelegate<Transform> onSingleTransformParameterEvent;
+    public delegate T GenericOneParameterDelegate<T>(T parameter);
+    public static event GenericOneParameterDelegate<int> onEventI;
+    public static event GenericOneParameterDelegate<float> onEventF;
+    public static event GenericOneParameterDelegate<string> onEventS;
+    public static event GenericOneParameterDelegate<Vector3> onEventV;
+    public static event GenericOneParameterDelegate<GameObject> onEventG;
+    public static event GenericOneParameterDelegate<Transform> onEventT;
+    public static event GenericOneParameterDelegate<object> onEventO;
+
+    /// <summary>
+    /// Generic Two Parameter Delegate, in the end of the event name there are first letter of each parameter
+    /// </summary>
+    /// <typeparam name="T">Generic T</typeparam>
+    /// <typeparam name="J">Generic J</typeparam>
+    /// <param name="parameterOne">First parameter</param>
+    /// <param name="parameterTwo">Second parameter</param>
+    /// <returns></returns>
+    public delegate T GenericTwoParameterDelegate<T,J>(T parameterOne, J parameterTwo);
+    public static event GenericTwoParameterDelegate<int, float> onEventIF;
+    public static event GenericTwoParameterDelegate<int, string> onEventIS;
+    public static event GenericTwoParameterDelegate<int, Vector3> onEventIV;
+    public static event GenericTwoParameterDelegate<int, GameObject> onEventIG;
+    public static event GenericTwoParameterDelegate<int, Transform> onEventIT;
+    public static event GenericTwoParameterDelegate<int, object> onEventIO;
+
+    /// <summary>
+    /// Generic Delegate with three parameter, in the end of the event name there are first letter of each parameter
+    /// </summary>
+    /// <typeparam name="T">Generic T </typeparam>
+    /// <typeparam name="J">Generic J </typeparam>
+    /// <typeparam name="K">Generic K </typeparam>
+    /// <param name="parameterOne">First parameter</param>
+    /// <param name="parameterTwo">Second parameter</param>
+    /// <param name="parameterThree">Third parameter</param>
+    /// <returns></returns>
+    public delegate T GenericThreeParameterDelegate<T, J, K>(T parameterOne, J parameterTwo, K parameterThree);
+    public static event GenericThreeParameterDelegate<int, string, float> onEventISF;
+    public static event GenericThreeParameterDelegate<int, string, Vector3> onEventISV;
+    public static event GenericThreeParameterDelegate<int, string, string> onEventISS;
+    public static event GenericThreeParameterDelegate<int, string, GameObject> onEventISG;
+    public static event GenericThreeParameterDelegate<int, string,Transform> onEventIST;
+    public static event GenericThreeParameterDelegate<int, string, object> onEventISO;
 
     private void Awake()
     {
@@ -169,6 +204,182 @@ public class EventManager : MonoBehaviour
     }
 
     #endregion PUZZLE EVENT CALL FUNCTIONS
+
+    #region GENERIC EVENT CALL FUNCTIONS
+
+    #region GENERIC SINGLE PARAMETER CALL FUNCTIONS
+    public void OnGenericEvent(int param)
+    {
+        if(DebugTable.EventDebug)
+        {
+            Debug.Log("Invoking Generic event with single parameter : " + param.GetType() + " " + param);
+        }
+        onEventI?.Invoke(param);
+    }
+
+    public void OnGenericEvent(float param)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with single parameter : " + param.GetType() + " " + param);
+
+        onEventF?.Invoke(param);
+    }
+
+    public void OnGenericEvent(string param)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with single parameter : " + param.GetType() + " " + param);
+
+        onEventS?.Invoke(param);
+    }
+
+    public void OnGenericEvent(Vector3 param)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with single parameter : " + param.GetType() + " " + param);
+
+        onEventV?.Invoke(param);
+    }
+
+    public void OnGenericEvent(GameObject param)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with single parameter : " + param.GetType() + " " + param);
+
+        onEventG?.Invoke(param);
+    }
+
+    public void OnGenericEvent(Transform param)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with single parameter : " + param.GetType() + " " + param);
+
+        onEventT?.Invoke(param);
+    }
+
+    public void OnGenericEvent(object param)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with single parameter : " + param.GetType() + " " + param);
+
+        onEventO?.Invoke(param);
+    }
+
+    #endregion SINGLE PARAMETER
+
+    #region GENERIC TWO PARAMETER CALL FUNCTIONS
+
+    public void OnGenericEvent(int param1, float param2)
+    {
+        if (DebugTable.EventDebug)
+        {
+            Debug.Log("Invoking Generic event with two parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2);
+        }
+        onEventIF?.Invoke(param1, param2);
+    }
+
+    public void OnGenericEvent(int param1, string param2)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with two parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2);
+
+        onEventIS?.Invoke(param1, param2);
+    }
+
+    public void OnGenericEvent(int param1 , Vector3 param2)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with two parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2);
+
+        onEventIV?.Invoke(param1, param2);
+    }
+
+    public void OnGenericEvent(int param1, GameObject param2)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with two parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2);
+
+        onEventIG?.Invoke(param1, param2);
+    }
+
+    public void OnGenericEvent(int param1 , Transform param2)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with two parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2);
+
+        onEventIT?.Invoke(param1, param2);
+    }
+
+    public void OnGenericEvent(int param1, object param2)
+    {
+        if (DebugTable.EventDebug)
+            Debug.Log("Invoking Geneic event with two parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2);
+
+        onEventIO?.Invoke(param1, param2);
+    }
+
+    #endregion GENERIC TWO PARAMETER CALL FUNCTIONS
+
+    #region GENERIC THREE PARAMETER CALL FUNCTIONS
+
+    public void OnGenericEvent(int param1, string param2, float param3)
+    {
+        if (DebugTable.EventDebug)
+        {
+            Debug.Log("Invoking Generic event with three parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2 + " " + param3.GetType() + " " + param3);
+        }
+        onEventISF?.Invoke(param1, param2, param3);
+    }
+
+    public void OnGenericEvent(int param1, string param2, string param3)
+    {
+        if (DebugTable.EventDebug)
+        {
+            Debug.Log("Invoking Generic event with three parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2 + " " + param3.GetType() + " " + param3);
+        }
+        onEventISS?.Invoke(param1, param2, param3);
+    }
+
+    public void OnGenericEvent(int param1, string param2, Vector3 param3)
+    {
+        if (DebugTable.EventDebug)
+        {
+            Debug.Log("Invoking Generic event with three parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2 + " " + param3.GetType() + " " + param3);
+        }
+        onEventISV?.Invoke(param1, param2, param3);
+    }
+    public void OnGenericEvent(int param1, string param2, Transform param3)
+    {
+        if (DebugTable.EventDebug)
+        {
+            Debug.Log("Invoking Generic event with three parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2 + " " + param3.GetType() + " " + param3);
+        }
+        onEventIST?.Invoke(param1, param2, param3);
+    }
+
+    public void OnGenericEvent(int param1, string param2, GameObject param3)
+    {
+        if (DebugTable.EventDebug)
+        {
+            Debug.Log("Invoking Generic event with three parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2 + " " + param3.GetType() + " " + param3);
+        }
+        onEventISG?.Invoke(param1, param2, param3);
+    }
+
+    public void OnGenericEvent(int param1, string param2, object param3)
+    {
+        if (DebugTable.EventDebug)
+        {
+            Debug.Log("Invoking Generic event with three parameter : " + param1.GetType() + " " + param1 + " " + param2.GetType() + " " + param2 + " " + param3.GetType() + " " + param3);
+        }
+        onEventISO?.Invoke(param1, param2, param3);
+    }
+
+
+    #endregion GENERIC THREE PARAMETER CALL FUNCTIONS
+
+
+    #endregion GENERIC EVENTS
 
     #endregion EVENT CALL FUNCTIONS
 
