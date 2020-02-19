e886688c86c4b2aefea421a739629a4fd5cb1a56
VelluVu <velluv@hotmail.fi>
2020-02-19 14:23:18 +0200

    Changes to saveables

---
+++ Assets/Scripts/SaveLoad/ISaveable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISaveable
+{
+
+    void UpdateMySaveable();
+
+    void LoadFromMySaveable();
+
+}
--- Assets/Scripts/SaveLoad/SaveablePuzzlePart.cs
+++ Assets/Scripts/SaveLoad/SaveablePuzzlePart.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class SaveablePuzzlePart
+public class SaveablePuzzlePart : Saveable
 {
     public bool partState;
-
-    public Vector3 position;
-    public Vector3 rotation;
-    public Vector3 scale;
 }
