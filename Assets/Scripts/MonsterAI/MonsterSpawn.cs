7861a63e97c5ec7fab71280912baa41a1ca9b685
VelluVu <velluv@hotmail.fi>
2020-06-15 11:35:26 +0300

    Added monster 0.2

--- Assets/Scripts/MonsterAI/MonsterSpawn.cs
+++ Assets/Scripts/MonsterAI/MonsterSpawn.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 /// <summary>
 /// @Author : Veli-Matti Vuoti
@@ -14,8 +15,7 @@ public class MonsterSpawn : MonoBehaviour
     public GameObject monster;
 
     public Transform[] spawnPoints;
-    public Transform[] validSpawnPoints;
-
+   
     public bool isSpawn;
     public bool isSpawnPressed;
 
@@ -35,7 +35,21 @@ public class MonsterSpawn : MonoBehaviour
     {
         if (monster == null)
         {
-            monster = Instantiate(monsterPrefab, transform.position, transform.rotation);
+            Vector3 spawnpos = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+            float currentDistance = Vector3.Distance(spawnpos, Player.instance.transform.position);
+
+            for (int i = 0; i < spawnPoints.Length; i++)
+            {
+                float distance = Vector3.Distance(spawnPoints[i].position, Player.instance.transform.position);
+
+                if (currentDistance < distance)
+                {
+                    spawnpos = spawnPoints[i].position;
+                    currentDistance = distance;
+                }
+            }
+
+            monster = Instantiate(monsterPrefab, spawnpos, transform.rotation);
         }
         else
         {
