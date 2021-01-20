38f56133eb42ff5eaedc00bfb677056edd59c00e
Emilie <emilie.lefrere10@gmail.com>
2021-01-20 11:27:44 +0200

    Artefact puzzle initial push

--- Assets/Scripts/Puzzles/ArtefactPuzzle/ArtefactPuzzle.cs
+++ Assets/Scripts/Puzzles/ArtefactPuzzle/ArtefactPuzzle.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class ArtefactPuzzle : Puzzle
 {
-    
+    [Tooltip("Artefact puzzle nodes/parts here in order.")]
+    [SerializeField]
+    public GameObject[] Nodes = new GameObject[4];
+
+    [Tooltip("Artefact puzzle materials to change on click.")]
+    [SerializeField]
+    private Material[] NodeMats = new Material[4];
+
+    private int[] NodeValues = new int[] { 2, 0, 3, 0 };
+
+
+    private GameObject renderer;
+
+    public void NodeThree()
+    {
+        
+        for (int i = 0; i <= 3; i++)
+        {
+            NodeValues[i] += 1;
+            if (NodeValues[i] > 3)
+                NodeValues[i] = 0;
+
+            renderer = GameObject.Find("Cubeee");
+            renderer.GetComponent<MeshRenderer>().material.color = Color.green;
+
+            Debug.Log(NodeValues[i]);
+        }
+
+    }
 }
--- Assets/Scripts/Puzzles/ArtefactPuzzle/ArtefactPuzzlePart.cs
+++ Assets/Scripts/Puzzles/ArtefactPuzzle/ArtefactPuzzlePart.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArtefactPuzzleLogic : Puzzlepart
+public class ArtefactPuzzlePart: Puzzlepart
 {
-    
+
 }
