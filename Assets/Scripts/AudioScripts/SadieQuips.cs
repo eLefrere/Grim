3617e843db4a2e6298c257a9d2a3ddb1123aac4b
Kosti <asd>
2021-01-26 23:24:10 +0200

    Sadie sound quips pre-script

---
+++ Assets/Scripts/AudioScripts/SadieQuips.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SadieQuips : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() // vai pitäiskö olla vaan sillon ku tapahtuu, ei joka freimi
+    {
+        if (/*jotain varmaan toisesta koodista ja aika moni eri juttu*/)
+        {
+            PlayQuip( _x_ );
+                
+        }
+    }
+
+    private void PlayQuip (_x_ quip_path)
+    {
+       
+        string quip_path;
+
+        switch ( _x_ )
+        {
+            case _1_: quip_path = "event:/VO/Player/Vo_Sadie_1 ManorEntry" break;
+            case _2_: quip_path = "event:/VO/Player/Vo_Sadie_ 2 UsedToLoveItHere" break;
+            
+            
+        }
+
+        FMODUnity.RuntimeManager.PlayOneShotAttached(quip_path, this.gameObject);
+    }
+}
