18093c47ec1d4b875188df602208814a342d1647
Emilie <emilie.lefrere10@gmail.com>
2021-02-04 14:18:58 +0200

    Added colliders to prevent player from teleporting downstairs

--- Assets/Scripts/Puzzles/ArtefactPuzzle/ArtefactPuzzle.cs
+++ Assets/Scripts/Puzzles/ArtefactPuzzle/ArtefactPuzzle.cs
@@ -12,33 +12,5 @@ public class ArtefactPuzzle : MonoBehaviour
     [SerializeField]
     private Material[] NodeMats = new Material[4];
 
-    private int[] NodeValues = new int[] { 2, 0, 3, 0 };
-
-    private Renderer renderer;
-    //private void Start()
-    //{
-    //    renderer = Nodes[2].GetComponent<Renderer>();
-    //    renderer.material.color = Color.green;
-    //}
-    private void OnMouseUp()
-    {
-        renderer = Nodes[2].GetComponent<Renderer>();
-        NodeThree();
-    }
-    public void NodeThree()
-    {
-
-        for (int i = 0; i <= 3; i++)
-        {
-            NodeValues[i] += 1;
-            if (NodeValues[i] > 3)
-                NodeValues[i] = 0;
-
-            //renderer.GetComponent<MeshRenderer>().material.color = Color.green;
-
-            Debug.Log(NodeValues[i]);
-        }
-        renderer.material.color = Color.green;
-
-    }
+    public int[] NodeValues = new int[] { 2, 0, 3, 0 };
 }
--- Assets/Scripts/TestScripts/Node3.cs
+++ Assets/Scripts/TestScripts/Node3.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node3 : MonoBehaviour
+public class Node3 : ArtefactPuzzle
 {
     ArtefactPuzzle node = new ArtefactPuzzle();
     private void OnMouseUp()
     {
-        node.NodeThree();
+        for (int i = 0; i <= 3; i++)
+        {
+            NodeValues[i] += 1;
+            if (NodeValues[i] > 3)
+                NodeValues[i] = 0;
+
+            
+
+            Debug.Log(NodeValues[i]);
+        }
+        GetComponent<MeshRenderer>().material.color = Color.green;
     }
 }
