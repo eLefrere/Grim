2bd7a633a7220b169273355b539cc734745f0681
SamHemming <32447304+SamHemming@users.noreply.github.com>
2021-06-15 17:27:04 +0300

    Artefact update
    artefactAttraction.cs
    -static list of all artefacts where every artefact adds and removes onceself
    -attracts to all objects on the list
    
    ArtefactGlower.cs (WIP)
    -static list of all artefacts finds closest distance and tweaks emission based on that

---
+++ Assets/Scripts/Puzzles/ArtefactAttraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ArtefactAttraction : MonoBehaviour
+{
+	static List<GameObject> artefacts = new List<GameObject>();
+	static float attractionStrength = 100;
+
+	private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		artefacts.Add(this.gameObject);
+	}
+
+	private void OnDestroy()
+	{
+		artefacts.Remove(this.gameObject);
+	}
+
+	private void FixedUpdate()
+	{
+		foreach(GameObject go in artefacts)
+		{
+			if (go == this.gameObject) continue;
+
+			Vector3 toTarget = go.transform.position - this.transform.position;
+			rb.AddForce(toTarget.normalized * (1 / toTarget.magnitude) * attractionStrength * Time.fixedDeltaTime);
+			//Debug.Log($"Direction: {toTarget.normalized}, Magnitude: {(1 / toTarget.magnitude)}, Force: {toTarget.normalized * (1 / toTarget.magnitude) * attractionStrength * Time.fixedDeltaTime}");
+		}
+	}
+}
---
+++ Assets/Scripts/Puzzles/ArtefactGlower.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtefactGlower : MonoBehaviour
+{
+	static List<GameObject> artefacts = new List<GameObject>();
+	
+
+	private Color baseEmissionColor = Color.white;
+
+	[SerializeField] private Material material;
+	[Range(0, 1)] public float minGlow = 0;
+	[Range(0, 1)] public float maxGlow = 1;
+	public float maxGlowDistance = 5;
+	public float glowStrength = 1;
+
+	private void Start()
+	{
+		artefacts.Add(this.gameObject);
+		if (!material) material = GetComponent<Renderer>().material;
+		baseEmissionColor = material.GetColor("_EmissionColor");
+	}
+
+	private void OnDestroy()
+	{
+		artefacts.Remove(this.gameObject);
+	}
+
+	private void Update()
+	{
+		float distance = Mathf.Infinity;
+
+		foreach (GameObject go in artefacts)
+		{
+			if (go == this.gameObject) continue;
+
+			float temp = Vector3.Distance(this.gameObject.transform.position, go.transform.position);
+			distance = Mathf.Min(temp, distance);
+		}
+
+		Debug.Log($"Shortest distance to another glower: {distance}");
+
+		float val01 = Mathf.Clamp01(Mathf.InverseLerp(0, maxGlowDistance, distance));
+
+		float glowVal = Mathf.Lerp(minGlow, maxGlow, val01);
+
+		Color col = baseEmissionColor * Mathf.LinearToGammaSpace(glowVal);
+
+		material.SetColor("_EmissionColor", col);
+	}
+}
