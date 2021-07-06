516f70e2108bc142d0b5993ea0bfec7a23eebfc8
SamHemming <32447304+SamHemming@users.noreply.github.com>
2021-07-06 18:02:44 +0300

    Menu and 3D buttons
    Button_3D.cs
    -simple 3d button to quit game and/or perform unityevent

---
+++ Assets/Scripts/Button_3D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Button_3D_Action { None, Quit }
+
+public class Button_3D : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.Events.UnityEvent OnClick;
+    [SerializeField] private Button_3D_Action buttonAction;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+            if (Physics.Raycast(ray, out RaycastHit hit, 10) && hit.transform.gameObject == gameObject)
+			{
+				Click();
+			}
+		}
+    }
+
+    public void Click()
+	{
+        OnClick?.Invoke();
+
+		switch (buttonAction)
+		{
+			case Button_3D_Action.Quit: //Quit the application or exit playmode in editor
+#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false;
+#else
+				Application.Quit();
+#endif
+				break;
+			case Button_3D_Action.None: //Doesn't do anything
+				break;
+			default:
+				break;
+		}
+	}
+}
